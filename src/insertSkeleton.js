81e538f218242c0580c7720b6544de7a200ffb87
siyue.huang <siyue.huang@medbanks.cn>
2020-10-14 17:06:03 +0800

    feat: 添加流程图 fix: 修复骨架屏动画播放时图片错位的问题

--- src/insertSkeleton.js
+++ src/insertSkeleton.js
@@ -15,67 +15,78 @@ const insertSkeleton = (skeletonImageBase64, options) => {
   const delayTimeToClose = options.delayTimeToClose;
 
   const content = `
-    <style id="${skeletonStyleNodeId}">
-      body {
-        overflow-x: hidden;
-        overflow-y: auto;
-      }
-      @keyframes flush {
-        0% {
-          left: -100%;
-        }
-        50% {
-          left: 0;
-        }
-        100% {
-          left: 100%;
-        }
-      }
-    </style>
-    <div class="${skeletonClass}" style="
-      animation: flush 2s linear infinite;
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      width: 100%;
-      z-index: 9999;
-      background: linear-gradient(to left,
-        rgba(255, 255, 255, 0) 0%,
-        rgba(255, 255, 255, .85) 50%,
-        rgba(255, 255, 255, 0) 100%);
-    "></div>
-    <div class="${skeletonClass}" style="
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      z-index: 9998;
-      background-repeat: no-repeat !important;
-      background-size: ${options.device ? '100% auto' : '1920px 1080px'} !important;
-      background-image: url(${skeletonImageBase64}) !important;
-      background-color: #FFFFFF !important;
-      background-position: center 0 !important;
-    "></div>
-    <script class="${skeletonClass}">
-      // 定义全局调用的钩子
-      window.SKELETON = {
-        destroy: function () { // 手动销毁骨架屏
-          var removes = Array.from(document.body.querySelectorAll('.${skeletonClass}'));
-          removes && removes.map(function(item){
-            document.body.removeChild(item);
-          });
-          document.head.removeChild(document.getElementById('${skeletonStyleNodeId}'));
-        }
-      };
-
-      // 在页面文件加载完毕之后，销毁骨架屏
-      window.addEventListener('load', function(){
-        setTimeout(function(){
-          window.SKELETON && SKELETON.destroy()
-        }, ${delayTimeToClose});
-      });
-    </script>`;
+  <html>
+    <head>
+        <style id="${skeletonStyleNodeId}">
+            body {
+                overflow-x: hidden;
+                overflow-y: auto;
+            }
+    
+            @keyframes flush {
+                0% {
+                    left: -100%;
+                }
+    
+                50% {
+                    left: 0;
+                }
+    
+                100% {
+                    left: 100%;
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <div class="${skeletonClass}" style="
+                    animation: flush 2s linear infinite;
+                    position: fixed;
+                    top: 0;
+                    bottom: 0;
+                    width: 100%;
+                    z-index: 9999;
+                    background: linear-gradient(
+                        to left,
+                        rgba(255, 255, 255, 0) 0%,
+                        rgba(255, 255, 255, 0.85) 50%,
+                        rgba(255, 255, 255, 0) 100%
+                    );
+                "></div>
+        <div class="${skeletonClass}" style="
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            z-index: 9998;
+            background-repeat: no-repeat !important;
+            background-size: ${options.device ? '100% auto' : '1920px 1080px'} !important;
+            background-image: url(${skeletonImageBase64}) !important;
+            background-color: #FFFFFF !important;
+            background-position: center 0 !important;
+        "></div>
+        <script class="${skeletonClass}">
+            // 定义全局调用的钩子
+            window.SKELETON = {
+                destroy: function () { // 手动销毁骨架屏
+                    var removes = Array.from(document.body.querySelectorAll('.${skeletonClass}'));
+                    removes && removes.map(function (item) {
+                        document.body.removeChild(item);
+                    });
+                    document.head.removeChild(document.getElementById('${skeletonStyleNodeId}'));
+                }
+            };
+    
+            // 在页面文件加载完毕之后，销毁骨架屏
+            window.addEventListener('load', function () {
+                setTimeout(function () {
+                    window.SKELETON && SKELETON.destroy()
+                }, ${ delayTimeToClose });
+            });
+        </script>
+    </body>
+  </html>`;
 
   const minifyContent = minify(content, {
     minifyCSS: true,
